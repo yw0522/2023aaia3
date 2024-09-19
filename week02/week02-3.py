9eb279dd1cdde9520815ca3d0b420fb00b6907db
yw0522 <144758555+yw0522@users.noreply.github.com>
2024-09-19 16:53:05 +0800

    Update week02-3.py

--- week02/week02-3.py
+++ week02/week02-3.py
@@ -1,11 +1,10 @@
-class Solution {
-public:
-    string mergeAlternately(string word1, string word2) {
-        string ans;
-        for(int i=0;i<word1.length() || i<word2.length(); i++){
-            if(i<word1.length()) ans+=word1[i];
-            if(i<word2.length()) ans+=word2[i];
-        }
-        return ans;
-    }
-};
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        N1=len(word1)
+        N2=len(word2)
+
+        ans=''
+        for i in range(max(N1,N2)):
+            if i<N1:ans+=word1[i]
+            if i<N2:ans+=word2[i]
+        return ans
